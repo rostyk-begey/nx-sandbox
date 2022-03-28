3c96d82f407197df47eacf5d35f6ef77060805e5
Rostyslav Begey <rostyk.begey@gmail.com>
2022-03-28 14:42:52 +0300

    chore: remove common library

--- libs/lib1/src/lib/lib1.spec.ts
+++ libs/lib1/src/lib/lib1.spec.ts
@@ -1,8 +1,7 @@
-import { common } from '@nx-sandbox/common';
 import { lib1 } from './lib1';
 
 describe('lib1', () => {
   it('should work', () => {
-    expect(lib1()).toEqual('lib1-v14' + common());
+    expect(lib1()).toEqual('lib1-v14');
   });
 });
--- libs/lib1/src/lib/lib1.ts
+++ libs/lib1/src/lib/lib1.ts
@@ -1,5 +1,3 @@
-import { common } from '@nx-sandbox/common';
-
 export function lib1(): string {
-  return 'lib1-v14' + common();
+  return 'lib1-v14';
 }
